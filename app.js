317cd5e2790f656d6c1dfd89e346fc19a432c02c
Asish Soudhamma <asishrs@gmail.com>
2016-05-30 13:39:37 -0700

    only frame guard

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const helmet = require('helmet')
+// const helmet = require('helmet')
 const frameguard = require('frameguard')
 const bodyParser = require('body-parser');
 const app = express();
@@ -11,10 +11,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
-app.use(helmet(frameguard({
+// app.use(helmet(frameguard({
+//   action: 'allow-from',
+//   domain: '*'
+// })));
+
+app.use(frameguard({
   action: 'allow-from',
   domain: '*'
-})));
+}))
 
 app.get('/', function(req, res) {
     res.render('index', {
