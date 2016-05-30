e887d614b1f631e96494aa4a4b3711496382f22c
Asish Soudhamma <asishrs@gmail.com>
2016-05-30 12:56:03 -0700

    removed body parser

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(allowCrossDomain)
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
-app.use(bodyParser.json({type: ['application/json', 'application/csp-report']}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.render('index', {
