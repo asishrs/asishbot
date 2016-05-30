aa43618a0dbd4da867f274da2c3d782026769041
Asish Soudhamma <asishrs@gmail.com>
2016-05-30 00:26:02 -0700

    css path update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
