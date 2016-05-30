8ef16725443744743d4a993e2cfb7beaf96c6c16
Asish Soudhamma <asishrs@gmail.com>
2016-05-30 13:57:45 -0700

    removed helmet added top to mm

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const express = require('express');
-// const helmet = require('helmet')
-const frameguard = require('frameguard')
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
@@ -11,15 +9,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
-// app.use(helmet(frameguard({
-//   action: 'allow-from',
-//   domain: '*'
-// })));
-
-app.use(frameguard({
-  action: 'allow-from',
-  domain: 'ALLOWALL'
-}))
 
 app.get('/', function(req, res) {
     res.render('index', {
