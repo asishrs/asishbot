4f7938fbb7026fb5d33fe2663d8a9e7615d068df
Asish Soudhamma <asishrs@gmail.com>
2016-05-30 13:35:19 -0700

    allow from

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(helmet(frameguard({
   action: 'allow-from',
-  domain: 'https://www.linkedin.com'
+  domain: 'http://*.asishrs.com'
 })));
 
 app.get('/', function(req, res) {
